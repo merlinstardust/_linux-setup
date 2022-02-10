e80f7df6b0fe91d658e7e0eb5433815e4d06c346
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-02-10 13:40:36 -0500

    Update run file

--- run.sh
+++ run.sh
@@ -3,25 +3,34 @@
 dotfilesDir=`pwd`/dotfiles
 ls -B dotfiles | while read filename; do
   if [ $filename != 'run.sh' ]; then
-    ln -fs $dotfilesDir/$filename $HOME/.$filename
+    rm -r $HOME/.$filename
+    ln -s $dotfilesDir/$filename $HOME/.$filename
   fi
 done
 echo "dotfiles copied"
+echo
 
 rm -f $HOME/.sources
 ln -s `pwd`/sources/ $HOME/.sources
 echo "sources copied"
+echo
 
 rm -f $HOME/.bin
 ln -s `pwd`/bin/ $HOME/.bin
-mv $HOME/bin $HOME/.bin
 echo "bin copied"
+echo
 
+rm "$HOME/Library/Application Support/Code/User/settings.json"
 ln -s `pwd`/vscode_settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 
 mkdir -p $HOME/code/
 mkdir -p $HOME/.tasks
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "brew installed"
+echo
 
 brew install emacs
+echo "emacs installed"
+echo
